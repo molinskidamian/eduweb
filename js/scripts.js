91769e33e2517fb445c5bf5b2903df3f1f85f652
Damian Molinski <tedeer@gmail.com>
2021-10-25 12:14:50 +0200

    Added roll-up and roll-down for .main-menu[0] element

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+(function () {
+    // roll-up/down menu
+    let showMenu = false;
+    const items = document.querySelectorAll(".main-nav--item");
+    const subMenu = document.querySelector(".main-nav--item > ul");
+
+    items[0].addEventListener("click", function () {
+        console.log("click");
+
+        if (showMenu === false) {
+            console.log("false");
+            subMenu.classList.add("main-nav--sub-menu");
+            showMenu = true;
+        } else if (showMenu === true) {
+            console.log("true");
+            showMenu = false;
+            subMenu.classList.remove("main-nav--sub-menu");
+        }
+    });
+})();
